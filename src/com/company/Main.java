e393eb388134438399d4da19b900249b2eecc65c
TheJesterX <56882887+TheJesterX@users.noreply.github.com>
2020-12-02 19:55:40 +0200

    User Input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,20 @@
 // Jacobus Rothmann
 
 package com.company;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         // A integer list to contain all the number that are multiples of 3 or 5
         ArrayList<Integer> numberList = new ArrayList<>();
         // Allows for a easy change in the max number
-        Integer number = 1000;
-	    for (int i = 1; i < number ; i++)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the max number that should not be included");
+        int max = sc.nextInt();
+        for (int i = 1; i < max ; i++)
         {
             // Checks if the number value of i are multiples of either 3 or 5
             if ((i %3 == 0) || (i %5 == 0))
@@ -18,13 +23,13 @@ public class Main {
                 numberList.add(i);
             }
         }
-	    // Calculates the sum of the numberList
+        // Calculates the sum of the numberList
         int sumValue = numberList.stream().mapToInt(Integer::intValue).sum();
-	    // Display all the numbers that are multiples of 3 or 5 below 1000
-	    // System.out.println(numberList);
+        // Display all the numbers that are multiples of 3 or 5 below 1000
+        // System.out.println(numberList);
         // Display the sum results
         System.out.println(
-                "The sum value of all natural number that are multiples of 3 or 5 below " + number + " : " + sumValue);
+                "The sum value of all natural number that are multiples of 3 or 5 below " + max + " : " + sumValue);
     }
 }
 
